c4b5ffa53a5020ccdb2fa01b582c30ee74f98dd2
ZFRenato <renatopscarlos@gmail.com>
2021-09-15 14:53:32 -0300

    Creação de Search User e busca de email e senha na tabela de usuário para login

--- app/model/user/userDataBase.js
+++ app/model/user/userDataBase.js
@@ -1,12 +1,12 @@
 const { connectionDataBase } = require('../database/connection');
 
 
+// INSERE NOVO USUÁRIO 
+
 async function insertUser (data){
 
     const newUser = {
-        nome: data.nome,
-        email: data.email,
-        senha: data.senha
+        ...data
     };
 
     try {
@@ -17,16 +17,59 @@ async function insertUser (data){
             id: result[0]
         }
         
-    } catch (error) {
-        
+    } catch (error) {        
         console.log(`INSERT NEW USER FALHOU Error: ${error.message}`);
     }
+
+}
+
+// BUSCA USUÁRIO NO BANCO POR EMAIL, SE EXISTIR RETORNA ID, CASO CONTRÁRIO RETORNA 0
+
+async function searchUser (data){
+
+    const email = { email: data.email}    
+
+    try {        
+        const result = await connectionDataBase('user').where(email);
+
+        return result.length == 0 ? 0 : result[0].id;
+
+    } catch (error) {        
+        console.log(`FALHOU erro: ${error.message}`);
+    }
+
+
+}
+
+
+// BUSCA USUÁRIO PARA LOGIN
+
+async function loginUser (data){
+
+    const user = { 
+        email: data.email,
+        senha: data.senha
+    }    
+
+    
+    try {        
+        const result = await connectionDataBase('user').where(user).select('id', 'nome');
+        
+        return result[0] == undefined ? 0 : result[0];         
+
+    } catch (error) {        
+        console.log(`FALHOU erro: ${error.message}`);
+    }
+
 
 }
 
 
 
 
+
 module.exports = {
-    insertUser
+    insertUser,
+    searchUser,
+    loginUser
 }
