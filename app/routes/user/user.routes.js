9ea737fdc704066992cc82713777651f3c186dee
ZFRenato <renatopscarlos@gmail.com>
2021-09-15 14:57:46 -0300

    criação de rota /login

--- app/routes/user/user.routes.js
+++ app/routes/user/user.routes.js
@@ -4,6 +4,7 @@ const userController = require('../../controller/user/userControler');
 const userRoutes = express.Router();
 
 userRoutes.post('/create', userController.createNewUser );
+userRoutes.post('/login', userController.loginUSer);
 
 module.exports = {
     userRoutes
