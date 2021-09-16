73c07055725c7ee2f358ae7346fb9e519ee98469
ZFRenato <renatopscarlos@gmail.com>
2021-09-16 19:57:00 -0300

    retirado rotas do arquivo do server

--- server.js
+++ server.js
@@ -1,23 +1,5 @@
 const app = require('./app/app')();
-const tmdb = require('./app/api/tmdb');
 
-
-app.get('/prodution',async (req, res )=>{
-    const params  = {
-        type: req.body.type,
-        id: req.body.id
-    }
-    res.send(await tmdb.getProdution({...params}));
-})
-
-app.get('/prodution/list', async (req, res)=>{
-    const params = {
-        type: req.body.type,
-        genre: req.body.genre
-    };
-
-    res.send( await tmdb.getProdutionsGen({...params}))
-})
 
 app.listen(app.get('port'),()=>{
     console.log(`Server on Port: ${app.get('port')}`);
